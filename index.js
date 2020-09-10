8f8bf9730d5078983be04b596d5ea9dfd33556eb
Dim <me@dimps.name>
2020-09-10 15:28:34 -0400

    Initial setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Hello world!")
+const fs = require('fs')
+const Ftp = require('ftp')
+const csv = require('fast-csv')
+const request = require('request')
+
+const ftp = new Ftp()
+ftp.on('ready', () => {
+    console.log('Ftp connected')
+})
+
+ftp.connect({
+	host: "feeds.futonland.com",
+	port: 21,
+	user: "constructorio",
+	password: "XFSrd0UFZSzG"
+});
